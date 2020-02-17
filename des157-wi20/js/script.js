d02e53a5100a2651a4561507d24f1e2e476a51c3
clarazhao101 <49996078+clarazhao101@users.noreply.github.com>
2020-02-16 22:09:22 -0800

    added processing sketch

---
+++ des157-wi20/js/script.js
@@ -0,0 +1,14 @@
+function setup() {
+  var myCanvas = createCanvas();
+  createCanvas(800, 250);
+  frameRate(8);
+  noStroke();
+  background('#3CB371');
+  myCanvas.parent(mySketch);
+}
+
+function draw() {
+
+    var circSize = random(40);
+  ellipse(mouseX, mouseY, circSize, circSize);
+}
